9659416ec4cc5e4233ef75da5c2cacc1f4b9c049
Fernando Bueno <fernando_jordi@hotmail.com>
2022-03-09 19:05:38 -0300

    adiciona requisito 6

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,12 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  const student = {
+const createStudent = (nome) => ({
     name: nome,
     feedback: () => 'Eita pessoa boa!',
-  };
-  return student;
-};
+  });
 
 module.exports = createStudent;
